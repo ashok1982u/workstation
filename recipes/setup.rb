f4ba046b489b0dbf50b73ef36167f9520addedd8
root <ashokms2017@gmail.com>
2018-02-20 14:50:06 +0000

    your commit msg

---
+++ recipes/setup.rb
@@ -0,0 +1,16 @@
+# ~/setup.rb
+
+package 'tree' do
+ action :install
+end
+
+package 'git' do
+ action :install
+end
+
+file '/etc/motd' do
+	content 'This server is the property of ...'
+	action :create
+	owner 'root'
+	group 'root'
+end
